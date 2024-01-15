a660470ed25065947ebd00cbcf551173ebfcddcf
Roberth Siqueira <roberth.siqueira@gmail.com>
2024-01-15 20:34:53 -0300

    fix: background context from datacontroller

--- VirtualTourist/Model/Persistence/CoreData/DataController.swift
+++ VirtualTourist/Model/Persistence/CoreData/DataController.swift
@@ -8,7 +8,6 @@ class DataController {
     // MARK: Properties
 
     let persistendContainer: NSPersistentContainer
-    let backgroundContext: NSManagedObjectContext
 
     var viewContext: NSManagedObjectContext {
         return persistendContainer.viewContext
@@ -18,25 +17,15 @@ class DataController {
 
     private init(modelName: String) {
         persistendContainer = NSPersistentContainer(name: modelName)
-        backgroundContext = persistendContainer.newBackgroundContext()
     }
 
     // MARK: API
 
-    func setupContext() {
-        viewContext.automaticallyMergesChangesFromParent = true
-        backgroundContext.automaticallyMergesChangesFromParent = true
-
-        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
-        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
-    }
-
     func load(completion: (() -> Void)? = nil) {
-        persistendContainer.loadPersistentStores { [weak self] storeDescription, error in
+        persistendContainer.loadPersistentStores { _, error in
             guard error == nil else {
                 fatalError(error!.localizedDescription)
             }
-            self?.setupContext()
             completion?()
         }
     }
